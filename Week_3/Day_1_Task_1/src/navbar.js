fc602cab9aea92de0b6ba9cf07acfe1a3d0edd83
wasifbinnasir <wasif_intern@netixsol.com>
2025-08-11 22:53:17 +0500

    resolving issue

--- Week_3/Day_1_Task_1/src/navbar.js
+++ Week_3/Day_1_Task_1/src/navbar.js
@@ -1,27 +1,24 @@
 
-// Drop Down Functionality
 const dropDown = document.querySelector("#dropdown")
 const separator = document.querySelector("#separator");
 const toggleDropDownBtn = document.querySelector("#toggle-dropdown-btn");
 const toggleDropDownImg = document.querySelector("#toggle-dropdown-img");
 
-let dropDownState = "close";
+let dropDownState = "false";
 
 function toggleDropDown () {
 
-    if (dropDownState == "close") {
+    if (dropDownState == "false") {
         separator.classList.remove("hidden")
         dropDown.classList.remove("hidden");
         toggleDropDownImg.src = "assets/hamburger-menu-close.svg";
-
-        dropDownState = "open";
+        dropDownState = "True";
     }
     else {
         separator.classList.add("hidden")
         dropDown.classList.add("hidden");
         toggleDropDownImg.src = "assets/hamburger-menu-open.svg";
-
-        dropDownState = "close";
+        dropDownState = "false";
     }
 }
 
@@ -29,7 +26,6 @@ toggleDropDownBtn.addEventListener("click", () => {
     toggleDropDown();
 });
 
-// Activate Links functionality
 const navUls = document.querySelectorAll("nav>ul, section>ul");
 
 navUls.forEach(ul => {
@@ -37,12 +33,10 @@ navUls.forEach(ul => {
         let target = event.target.closest("li[data-active]");
         if (!target) return;
 
-        // Remove active from all links in this ul
         ul.querySelectorAll("li[data-active]").forEach(li => {
             li.dataset.active = "false";
         });
 
-        // Set active to the clicked link
         target.dataset.active = "true";
     });
 });
